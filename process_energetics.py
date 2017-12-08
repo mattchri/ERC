included plotting code and updated averaging method identified bug in instantaneous code but have not corrected it.

process_energetics.py
@@ -1,3 +1,7 @@
+#CURRENTLY, THE INSTANTANEOUS FIELDS ARE NOT CORRECT BECAUSE
+#ONLY 00:00 AND 12:00 ARE BEING USED TO CONSTRUCT THE MEAN
+#CAUSING LARGE VARIATION ACROSS LONGITUDE.
+
 #---------------------------------------------------------------
 #TOP LEVEL CODE
 #---------------------------------------------------------------
@@ -15,62 +19,119 @@
 #dFa: horizontal divergence of energy out of the column by transpoft in the atmosphere
 #
 #Example
-#python2.7 -i process_energetics.py 2017 8 14 fv1.0
+#python2.7 -i process_energetics.py
 #
 # 02/11/17, MC: upload initial version of the code to the repo
+# 08/11/17, MC: included plotting code and updated averaging method
+#               identified bug in instantaneous code but have not
+#               corrected it.
 #---------------------------------------------------------------
 import sys
 import os
-from netCDF4 import Dataset
 from subroutines import *
-import matplotlib.pyplot as plt
-import matplotlib.text as text
+from plotting import *
 
-#---------------------------------------------------------------
-# input variables read from command line
-#---------------------------------------------------------------
-year  = int(sys.argv[1])
-month = int(sys.argv[2])
-dom   = int(sys.argv[3])
-version   = sys.argv[4]
+figpath = '/home/users/mchristensen/Desktop/ERC/'
+figpath = '/group_workspaces/cems/cloud_ecv/public/temp_transfer/erc/'
 
-#---------------------------------------------------------------
-#Paths
-#---------------------------------------------------------------
-outpath = '/group_workspaces/jasmin2/aopp/mchristensen/erc/energetics/'
-testFile='/group_workspaces/cems2/nceo_generic/model_data/ERA_INTERIM/ERA_INTERIM_ENERGETICS_'+str(year).zfill(4)+str(month).zfill(2)+'.nc'
-#---------------------------------------------------------------
+#Compute Fluxes based on monthly, seasonal, and annual
 
-#TP = read_ecmwf_variable('TP',year,month,dom,testFile)
-#TP = read_ecmwf_variable('TP',year,month,dom,'')
+years = [2015]
+months = [1,2,3,4,5,6,7,8,9,10,11,12]
+for tY in range(len(years)):
+    year=years[tY]
+    #Fetch File
+    ecmwfFile ='/group_workspaces/cems2/nceo_generic/model_data/ERA_INTERIM/ERA_INTERIM_ENERGETICS_monthly_'+str(year).zfill(4)+'.nc'
+    data_month = []
+    for tM in range(len(months)):
+        month=months[tM]
+        dom=15
+        print('Processing: ',str(months[tM]).zfill(2))
+        flux_month = process_ecmwf_variables(year,month,dom,ecmwfFile)['daily']
+        data_month.append(flux_month)
+        
+varNames = np.array([x['name'] for x in data_month[0]])
+VARS = ['Ra','LP','SH','dFa','H','Ea','toa_swdn','toa_swup','toa_lwup','boa_lwup','boa_lwdn','boa_swdn','boa_swup']
+VARS_PLOT = [r'$R_a$','LP','SH',r'$\Delta$'+r'$F_a$',r'$H$',r'$E_a$','toa_swdn','toa_swup','toa_lwup','boa_lwup','boa_lwdn','boa_swdn','boa_swup']
+VARS_UNIT = r'W/m$^{2}$'
+VARS_titl = [VARS_PLOT[0]+' ('+VARS_UNIT+')', VARS_PLOT[1]+' ('+VARS_UNIT+')',
+             VARS_PLOT[2]+' ('+VARS_UNIT+')', VARS_PLOT[3]+' ('+VARS_UNIT+')',
+             VARS_PLOT[4]+' ('+VARS_UNIT+')', VARS_PLOT[5]+' ('+VARS_UNIT+')',
+             VARS_PLOT[6]+' ('+VARS_UNIT+')', VARS_PLOT[7]+' ('+VARS_UNIT+')',
+             VARS_PLOT[8]+' ('+VARS_UNIT+')', VARS_PLOT[9]+' ('+VARS_UNIT+')',
+             VARS_PLOT[10]+' ('+VARS_UNIT+')', VARS_PLOT[11]+' ('+VARS_UNIT+')',
+             VARS_PLOT[12]+' ('+VARS_UNIT+')']
+VARS_mins = [-400, 0   , -60, -200, -400, -100, 0  ,0  ,100, 100, 100, 0  ,0]
+VARS_maxs = [-100, 300 ,  60,  200,  400,  100, 420,300,300, 500, 400, 300,200]
+nvars = len(VARS)
+lats = ((flux_month[0])['data'])[:]
+lons = ((flux_month[1])['data'])[:]
 
-#ecmwf_flux = ( process_ecmwf_variables(year,month,dom,'') )['daily']
-ecmwf_flux = ( process_ecmwf_variables(year,month,dom,testFile) )['daily']
-varNames = np.array([x['name'] for x in ecmwf_flux])
 
-#Fetch array size
-sz=(ecmwf_flux[10])['data'].shape
-xDim=sz[0]
-yDim=sz[1]
-print(xDim,yDim)
+#Fetch dimensions
+ecmwf_flux = data_month[0]
+index = ((np.where(varNames == 'Ra'))[0])[0]
+tmpdata = (ecmwf_flux[index])['data']
+xdim = (tmpdata.shape)[0]
+ydim = (tmpdata.shape)[1]
 
-VARS = ['Ra','LP','SH','dFa','H']
-VARS_PLOT = [r'$R_a$','LP','SH',r'$\Delta$'+r'$F_a$',r'$H$']
-ARR  = np.zeros( [xDim,len(VARS)] )
-for i in range(len(VARS)):
-    index = ((np.where(varNames == VARS[i]))[0])[0]
-    tmp=(ecmwf_flux[index])['data']
-    ARR[:,i] = np.mean(tmp, axis=1)
+#Monthly Mean
+varData_month = np.zeros( [xdim, ydim, 12, nvars] )
+for tM in range(len(months)):
+    ecmwf_flux = data_month[tM]
+    for iV in range(nvars):
+        index = ((np.where(varNames == VARS[iV]))[0])[0]
+        varData_month[:,:,tM,iV]=(ecmwf_flux[index])['data']
 
-lat = ((ecmwf_flux[0])['data'])[:]
-plt.plot(lat, ARR[:,0], 'r--', 
-         lat, ARR[:,1], 'b--', 
-         lat, ARR[:,2], 'g--', 
-         lat, ARR[:,3], 'y--',
-         lat, ARR[:,4], 'm--')
+#Annual Mean
+print('Averaging over Year')
+varData_annual = np.zeros( [xdim, ydim, nvars] )
+for iV in range(nvars):
+    varData_annual[:,:,iV] = np.mean(varData_month[:,:,:,iV],axis=2)
+
+#Seasonal Mean
+print('Averaging over Season')
+sid = np.array( [ [12,1,2], [3,4,5 ] , [6,7,8], [9,10,11] ] )
+varData_season = np.zeros( [xdim, ydim, 4, nvars] )
+for iS in range(4):
+    for iV in range(nvars):
+        varData_season[:,:,iS,iV] = np.mean(varData_month[:,:,sid[iS,:]-1,iV], axis=2)
+
+ARR = np.mean( varData_annual, axis=1 )
+#Plot Annual Meridional Distribution
+plt.plot(lats, ARR[:,0], 'r--', 
+         lats, ARR[:,1], 'b--', 
+         lats, ARR[:,2], 'g--', 
+         lats, ARR[:,3], 'y--')
 plt.ylabel('Watts/Square Meter')
 plt.xlabel('Latitude')
-plt.legend((VARS_PLOT),loc='upper left', shadow=True)
+plt.legend((VARS_PLOT[0:3]),loc='upper left', shadow=True)
 plt.title('Atmospheric Energy Budget')
-plt.show()
-#plt.savefig('/home/users/mchristensen/Desktop/test.png')
+#plt.show()
+plt.savefig(figpath+'merid_constrained_annual.png')
+plt.close()
+
+#Plot Annual Meridional Distribution
+plt.plot(lats, ARR[:,0], 'r--', 
+         lats, ARR[:,1], 'b--', 
+         lats, ARR[:,2], 'g--', 
+         lats, ARR[:,3], 'y--',
+         lats, ARR[:,4], 'm--',
+         lats, ARR[:,5], 'b-')
+plt.ylabel('Watts/Square Meter')
+plt.xlabel('Latitude')
+plt.legend((VARS_PLOT[0:6]),loc='upper left', shadow=True)
+plt.title('Atmospheric Energy Budget')
+#plt.show()
+plt.savefig(figpath+'merid_annual.png')
+plt.close()
+
+#Geographic distribution
+for iV in range(nvars):
+    data = varData_annual[:,:,iV]
+    minv = VARS_mins[iV]
+    maxv = VARS_maxs[iV]
+    titl = VARS_titl[iV]
+    figtitl = figpath+'global_annual_'+VARS[iV]+'.png'
+    plot_map(lons,lats,data,minv,maxv,titl,figtitl)
+      

subroutines.py
@@ -12,8 +12,13 @@ from jdcal import *
 import sys
 import numpy as np
 from netCDF4 import Dataset
+import netCDF4
 from datetime import datetime
 import pdb
+import numpy.ma as ma
+#import iris
+#import datetime
+#iris.FUTURE.cell_datetime_objects = True
 
 def doy_to_month(doy,year):
     #fetch julian day
@@ -74,112 +79,6 @@ def extract_time_ecmwf(fname):
 
 
 #-----------------------------------------------------------------
-#COLLOCATE_FILES
-#Input the base SEVIRI files (merged output in this case) and
-#all other file types (e.g. imerg, ECMWF, ect) to collocate to SEVIRI
-#
-#Input list of files for each data product
-#Output list of collocated files from each product
-#-----------------------------------------------------------------
-def collocate_files(infiles,dt):
-    #SEVIRI Base files Information
-    BaseDict     = infiles[0] #by default zeroth element
-    BaseFileInfo = BaseDict['files']
-    BaseFiles = [item['filename'] for item in BaseFileInfo]
-    BaseJDAY = [item['JDAY'] for item in BaseFileInfo]
-    BasePath = BaseDict['path']
-
-    #Define Matched Array
-    nFileTypes = len(infiles)
-    nFiles = len(BaseFiles)
-    print '# Data Products: ',nFileTypes
-    print '# Base (collocation) Files: ',nFiles
-    matchedFiles = np.empty([nFileTypes,nFiles], dtype="S512")
-    for iT in range(nFiles):
-        matchedFiles[0,iT] = BasePath+BaseFiles[iT]
-
-    i=1 #initialised value required 1st file type in for loop
-    #Loop over each data product
-    for iT in range(nFileTypes-1):
-        TMPDict     = infiles[i] #by default zeroth element
-        TMPFileInfo = TMPDict['files']
-        TMPFiles    = [item['filename'] for item in TMPFileInfo]
-        TMPJDAY     = [item['JDAY'] for item in TMPFileInfo]
-        TMPJDAY1 = [iii[1] for iii in TMPJDAY]
-        TMPPath  = TMPDict['path']
-        y = np.asarray(TMPJDAY1)
-
-        #Loop over each file
-        for j in range(nFiles):
-            x = float((BaseJDAY[j])[1]) #julian day temp var
-            fid = np.where(abs(y-x) == min(abs(y-x)))
-            z = int(fid[0])
-            #print i,j,BaseFiles[j],'  ',TMPFiles[z]
-            matchedFiles[ i, j ] = TMPPath + TMPFiles[z]
-
-        i=i+1 #start with first data product (+1)
-    return matchedFiles
-
-#-----------------------------------------------------------------
-#COLLOCATE_IMERG_SEVIRI
-#Routine collocates the IMERG lat/lon to the SEVIRI lat/lon grid
-#The top-level code produces a save file so this only should be run
-#one time since the lat/lon fields are invariant for both sensors
-#Input
-# lonIM, latIM, lonSV, latSV
-#Output
-#index for longitude and latitude arrays
-#-----------------------------------------------------------------
-def collocate_imerg_seviri(lonIM,latIM,lonSV,latSV):
-    print('Collocating IMERG to SEVIRI (~1hr processing time)')
-    xDim = (lonSV.shape)[0]
-    yDim = (latSV.shape)[1]
-    LATidIM = np.zeros( (xDim, yDim) )
-    LONidIM = np.zeros( (xDim, yDim) )
-    for iX in range(xDim):
-        print(iX)
-        for iY in range(yDim):
-            tmpLat = latSV[iX,iY]
-            tmpLon = lonSV[iX,iY]
-            if tmpLon > -999. and tmpLat > -999.:
-                LONidIM[iX,iY] = int(((np.where(abs(lonIM-tmpLon) == min(abs(lonIM-tmpLon))) )[0])[0])
-                LATidIM[iX,iY] = int(((np.where(abs(latIM-tmpLat) == min(abs(latIM-tmpLat))) )[0])[0])
-            else:
-                LONidIM[iX,iY] = -999.
-                LATidIM[iX,iY] = -999.                   
-    IMERG_ID = {'latid':LATidIM, 'lonid':LONidIM, 'xdim':xDim, 'ydim':yDim}
-    return IMERG_ID
-
-
-#Create NetCDF file
-def make_netcdf_file(ncname,vName,vData,vLong,vStan,vUnit,vFill):
-    print('Creating: '+ncname)
-    sz = vData.shape
-    nVars= sz[0]
-    xDim = sz[1]
-    yDim = sz[2]
-
-    f = Dataset(ncname,'w', format='NETCDF4_CLASSIC')
-    f.createDimension('xdim', xDim)
-    f.createDimension('ydim', yDim)
-    
-    for iV in range(len(vName)):
-        tmpstr = vName[iV]
-        temp = f.createVariable(tmpstr, 'f4', ('xdim','ydim'), zlib=True)
-        temp[:,:] = vData[iV,:,:]
-        temp.standard_name  = vStan[iV]
-        temp.long_name  = vLong[iV]
-        temp.units = vUnit[iV]
-        temp.FillValue = vFill[iV]
-
-    #global attributes
-    today = datetime.today()
-    f.description = "Satellite Model Integrated Product"
-    f.history = "Created " + today.strftime("%d/%m/%y")
-    f.close()
-
-
-#-----------------------------------------------------------------
 #Read Accumulated (gafs) ECWMF variable from /badc/ on JASMIN
 #Inputs
 #variable name (e.g. 'TP' total precipitation)
@@ -189,37 +88,32 @@ def make_netcdf_file(ncname,vName,vData,vLong,vStan,vUnit,vFill):
 #Output
 #ARR [accumulated field]
 #-----------------------------------------------------------------
-def read_ecmwf_variable(vname,year,month,dom,testfile):
+def read_ecmwf_variable(vname,year,month,dom,inputfile):
 
     #Read Downloaded ECWMF Data
-    if(len(testfile) > 1):
-        ncfile = Dataset( testfile, mode='r')
-        time = ncfile.variables['time'][:]
-        #units: hours since 1900-01-01 00:00:0.0
-        jdayST = gcal2jd( 1900, 1, 1 )
-        jday   = (jdayST[0],jdayST[1]+time/24.)
-        sort_index = np.argsort(jday[1])
-        jday_sorted= (jdayST[0], (jday[1])[sort_index] )
-        alljday = jday_sorted[1]
+    if(len(inputfile) > 1):
+        ncfile = Dataset( inputfile, mode='r')
+        time = ncfile.variables['time']
+        dates = netCDF4.num2date(time[:], time.units, time.calendar)
+        tmpTime = time[:]
+        t_months = np.array([x.month for x in dates])
+        t_days = np.array([x.day for x in dates])
+        t_hours = np.array([x.hour for x in dates])
+        t_index = (np.where( t_months == month))[0]
+        
+        #Now sort the index
+        tmpsortID  = np.asarray(np.argsort(time[t_index]))
+        sort_index = t_index[tmpsortID[:]]
 
-        #Find indices for corresponding input day
-        jdayInput = gcal2jd(year,month,dom)
-        injday = jdayInput[1]
-
-        ecmwf_tID = np.where( alljday.astype(int) == int(injday))
-
-        id_sorted = sort_index[ecmwf_tID]
-
-        ncfile = Dataset( testfile, mode='r')
         scaleF  = ncfile.variables[vname.lower()].scale_factor
         add_off = ncfile.variables[vname.lower()].add_offset
         fillVal = ncfile.variables[vname.lower()]._FillValue
         unitStr = ncfile.variables[vname.lower()].units
+        ARR = (ncfile.variables[vname.lower()])[sort_index,:,:]
 
-        ARR = (ncfile.variables[vname.lower()])[id_sorted[:],:,:]
 
     # Read ECWMF Data from Jasmin
-    if(len(testfile) == 0):
+    if(len(inputfile) == 0):
         path_ecmwf = '/badc/ecmwf-era-interim/data/ga/fs/'+str(year).zfill(4)+'/'+str(month).zfill(2)+'/'+str(dom).zfill(2)+'/' 
         prefix = str(year).zfill(4)+str(month).zfill(2)+str(dom).zfill(2)
         ftimes = ['0003','0006','0009','0012','1203','1206','1209','1212']
@@ -232,15 +126,23 @@ def read_ecmwf_variable(vname,year,month,dom,testfile):
             ARR[i,:,:] = (ncfile.variables[vname][:])[0,0,:,:]
 
 
-    #if(vname == 'ishf'):
+    #if(vname == 'p83.162'):
     #    pdb.set_trace()
 
-    if((np.asarray(ARR[:,0,0]))[1] == fillVal):
+    #if(vname == 'TISR'):
+    #    pdb.set_trace()
+
+    if(ma.is_masked(((ARR[:,0,0]))[0]) == False):
         print('Instantaneous: '+vname)
-        aARR = instantaneous_ecmwf_3hr(ARR)
-    if((np.asarray(ARR[:,0,0]))[1] != fillVal):
+        tmpARR = ARR[ [0,4],:,:]
+        print(tmpARR[:,120,240])
+        aARR = instantaneous_ecmwf_3hr(tmpARR)
+        print(aARR[:,120,240])
+
+    if(ma.is_masked(((ARR[:,0,0]))[0]) == True):
         print('Accumulated: '+vname)
-        aARR = accumulate_ecmwf_3hr(ARR)
+        tmpARR = ARR[:,:,:]
+        aARR = accumulate_ecmwf_3hr(tmpARR)
 
 
     #Daily means
@@ -266,70 +168,74 @@ def accumulate_ecmwf_3hr(ARR):
     aARR[5,:,:] = ( ARR[5,:,:]-ARR[5,:,:]) / tstep
     aARR[6,:,:] = ( ARR[6,:,:]-ARR[6,:,:]) / tstep
     aARR[7,:,:] = ( ARR[7,:,:]-ARR[7,:,:]) / tstep
+    aARR[:,:,:] = (ARR[4,:,:]+ARR[8,:,:]) / (tstep*8.)
     return aARR    
 
 def instantaneous_ecmwf_3hr(ARR):
     #Express quantity as instantaneous
     sz=ARR.shape
-    aARR     = np.empty( [sz[0],sz[1],sz[2]] )
+    aARR     = np.empty( [9,sz[1],sz[2]] )
     aARR[0,:,:] = ARR[0,:,:]
     aARR[1,:,:] = ARR[0,:,:]
-    aARR[2,:,:] = ARR[2,:,:]
-    aARR[3,:,:] = ARR[2,:,:]
-    aARR[4,:,:] = ARR[4,:,:]
-    aARR[5,:,:] = ARR[4,:,:]
-    aARR[6,:,:] = ARR[6,:,:]
-    aARR[7,:,:] = ARR[6,:,:]
+    aARR[2,:,:] = ARR[0,:,:]
+    aARR[3,:,:] = ARR[0,:,:]
+    aARR[4,:,:] = ARR[1,:,:]
+    aARR[5,:,:] = ARR[1,:,:]
+    aARR[6,:,:] = ARR[1,:,:]
+    aARR[7,:,:] = ARR[1,:,:]
+    aARR[8,:,:] = ARR[1,:,:]
+    aARR[:,:,:] = np.mean(ARR,axis=0)
     return aARR    
 
 
-def process_ecmwf_variables(year,month,dom,testfile):
+def process_ecmwf_variables(year,month,dom,inputfile):
     #Read Data
-    STRD = read_ecmwf_variable('STRD',year,month,dom,testfile)
-    TISR = read_ecmwf_variable('TISR',year,month,dom,testfile)
-    TSR  = read_ecmwf_variable('TSR',year,month,dom ,testfile)
-    TTR  = read_ecmwf_variable('TTR',year,month,dom ,testfile)
-    SSRD = read_ecmwf_variable('SSRD',year,month,dom,testfile)
-    SSR  = read_ecmwf_variable('SSR',year,month,dom ,testfile)
-    STRD = read_ecmwf_variable('STRD',year,month,dom,testfile)
-    STR  = read_ecmwf_variable('STR',year,month,dom ,testfile)
-    TP   = read_ecmwf_variable('TP',year,month,dom  ,testfile)
-    SSHF = read_ecmwf_variable('SSHF',year,month,dom,testfile)
+    STRD = read_ecmwf_variable('STRD',year,month,dom,inputfile)
+    TISR = read_ecmwf_variable('TISR',year,month,dom,inputfile)
+    TSR  = read_ecmwf_variable('TSR',year,month,dom ,inputfile)
+    TTR  = read_ecmwf_variable('TTR',year,month,dom ,inputfile)
+    SSRD = read_ecmwf_variable('SSRD',year,month,dom,inputfile)
+    SSR  = read_ecmwf_variable('SSR',year,month,dom ,inputfile)
+    STRD = read_ecmwf_variable('STRD',year,month,dom,inputfile)
+    STR  = read_ecmwf_variable('STR',year,month,dom ,inputfile)
+    TP   = read_ecmwf_variable('TP',year,month,dom  ,inputfile)
+    SSHF = read_ecmwf_variable('SSHF',year,month,dom,inputfile)
     sz=SSHF['data'].shape
     DIV_DRY_STATIC_ENERGY_data = np.zeros( [sz[0],sz[1],sz[2]] )
     DIV_DRY_STATIC_ENERGY_daily = np.zeros( [sz[1],sz[2]] )
 
-    if(len(testfile) > 1):
+    if(len(inputfile) > 1):
         #NOTE THESE QUANTITIES ARE NOT ACCUMULATED!!!
-        p82 = read_ecmwf_variable('p82.162',year,month,dom,testfile)
-        p83 = read_ecmwf_variable('p83.162',year,month,dom,testfile)
-        p85 = read_ecmwf_variable('p85.162',year,month,dom,testfile)
+        p82 = read_ecmwf_variable('p82.162',year,month,dom,inputfile)
+        p83 = read_ecmwf_variable('p83.162',year,month,dom,inputfile)
+        p85 = read_ecmwf_variable('p85.162',year,month,dom,inputfile)
         #pdb.set_trace()
         DIV_DRY_STATIC_ENERGY_data  = (p82['data']+p83['data']+p85['data']   )
         DIV_DRY_STATIC_ENERGY_daily = (p82['daily']+p83['daily']+p85['daily'])
         #pdb.set_trace()
-        ISHF = read_ecmwf_variable('ishf',year,month,dom,testfile)
+        ISHF = read_ecmwf_variable('ishf',year,month,dom,inputfile)
         ISHF_data = ISHF['data'] * 3. * 3600.
         ISHF_daily = ISHF['daily'] * 3. * 3600.
 
     # Calculate Fluxes
     L = 2.5e6 #latent heat of vaporization at 0C (J/kg)
     boa_lwdn = STRD['data']
-    boa_lwup = STR['data'] - STRD['data']
+    boa_lwup = STRD['data'] - STR['data']
     boa_swdn = SSRD['data']
-    boa_swup = SSR['data'] - SSRD['data']
-    toa_lwup = TTR['data']
+    boa_swup = SSRD['data'] - SSR['data']
+    toa_lwup = 0. - TTR['data']
     toa_swdn = TISR['data']
-    toa_swup = TSR['data'] - TISR['data']
+    toa_swup = TISR['data'] - TSR['data']
     prate    = TP['data'] * 1000. * 3600. #(m/s * 3600. * 1000mm/m --> mm/hr)
-    #SH     = 0. - SSHF['data']  #not sure if this is right!!!!!
-    SH     = 0. - ISHF_data
+    SH     = 0. - SSHF['data']  #not sure if this is right!!!!!
+    #SH     = 0. - ISHF_data
     LP       = L * prate *(1/3600.) #(J/kg * mm/hr * 1kg/m2 * 1hr/3600s --> W/m2)
     Rtoa = toa_swdn - toa_swup + 0.       - toa_lwup
     Rs   = boa_swdn - boa_swup + boa_lwdn - boa_lwup
     Ra   = Rtoa - Rs
     dFa  = Ra + LP + SH
     H    = DIV_DRY_STATIC_ENERGY_data
+    Ea   = Ra + LP + SH - H
     ecmwf_flux_hourly = [ {'data':STRD['lat'], 'name':'lat',  'long':'latitude'},
                           {'data':STRD['lon'], 'name':'lon',  'long':'longitude'},
                           {'data':boa_lwdn, 'name':'boa_lwdn','long':'bottom of atmosphere downwelling longwave radiative flux'},
@@ -346,24 +252,29 @@ def process_ecmwf_variables(year,month,dom,testfile):
                           {'data':Rs, 'name':'Rs','long':'bottom of atmosphere downwelling longwave radiative flux'},
                           {'data':Ra, 'name':'Ra','long':'bottom of atmosphere downwelling longwave radiative flux'},
                           {'data':dFa, 'name':'dFa','long':'bottom of atmosphere downwelling longwave radiative flux'},
-                          {'data':H  , 'name':'H','long':'bottom of atmosphere downwelling longwave radiative flux'}]
+                          {'data':H  , 'name':'H','long':'bottom of atmosphere downwelling longwave radiative flux'},
+                          {'data':Ea , 'name':'Ea','long':'time rate of change of the energy content of an atmospheric column of unit horizontal area extending from the surface to the top of the atmosphere'}]
 
+
+
+#might need to reverse the order of STR & STRD and SSR & SSRD!!!!
     boa_lwdn = STRD['daily']
-    boa_lwup = STR['daily'] - STRD['daily']
+    boa_lwup = STRD['daily'] - STR['daily']
     boa_swdn = SSRD['daily']
-    boa_swup = SSR['daily'] - SSRD['daily']
-    toa_lwup = TTR['daily']
+    boa_swup = SSRD['daily'] - SSR['daily']
+    toa_lwup = 0. - TTR['daily']
     toa_swdn = TISR['daily']
-    toa_swup = TSR['daily'] - TISR['daily']
+    toa_swup = TISR['daily'] - TSR['daily']
     prate    = TP['daily'] * 1000. * 3600. #(m/s * 3600. * 1000mm/m --> mm/hr)
-    #SH     = 0. - SSHF['daily']
-    SH     = 0. - ISHF_daily
+    SH     = 0. - SSHF['daily']
+    #SH     = 0. - ISHF_daily
     LP       = L * prate *(1/3600.) #(J/kg * mm/hr * 1kg/m2 * 1hr/3600s --> W/m2)
     Rtoa = toa_swdn - toa_swup + 0.       - toa_lwup
     Rs   = boa_swdn - boa_swup + boa_lwdn - boa_lwup
     Ra   = Rtoa - Rs
     dFa  = Ra + LP + SH
     H = DIV_DRY_STATIC_ENERGY_daily
+    Ea   = Ra + LP + SH - H
     ecmwf_flux_daily = [ {'data':STRD['lat'], 'name':'lat',  'long':'latitude'},
                           {'data':STRD['lon'], 'name':'lon',  'long':'longitude'},
                           {'data':boa_lwdn, 'name':'boa_lwdn','long':'bottom of atmosphere downwelling longwave radiative flux'},
@@ -380,7 +291,10 @@ def process_ecmwf_variables(year,month,dom,testfile):
                           {'data':Rs, 'name':'Rs','long':'bottom of atmosphere downwelling longwave radiative flux'},
                           {'data':Ra, 'name':'Ra','long':'bottom of atmosphere downwelling longwave radiative flux'},
                           {'data':dFa, 'name':'dFa','long':'bottom of atmosphere downwelling longwave radiative flux'},
-                          {'data':H  , 'name':'H','long':'bottom of atmosphere downwelling longwave radiative flux'}]
+                          {'data':H  , 'name':'H','long':'bottom of atmosphere downwelling longwave radiative flux'},
+                          {'data':Ea , 'name':'Ea','long':'time rate of change of the energy content of an atmospheric column of unit horizontal area extending from the surface to the top of the atmosphere'}]
 
     ecmwf_flux = {'hourly':ecmwf_flux_hourly,'daily':ecmwf_flux_daily}
     return ecmwf_flux
+
+